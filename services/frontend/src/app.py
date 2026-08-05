552232a1b7163ae589a63093fbddfc8039f5315a
Tchana R <ruphustchana@yahoo.com>
2026-08-05 16:57:32 +0200

    feat:asp frontend projet initialized

---
+++ services/frontend/src/app.py
@@ -0,0 +1,7 @@
+"""
+starting point ASP frontend service with streamlit
+"""
+
+import streamlit as st
+
+st.set_page_config(page_title="ASP - Accident Severity Predictor", page_icon="🚗", layout="wide", initial_sidebar_state="collapsed")
---
+++ services/frontend/src/pages/home.py
@@ -0,0 +1,12 @@
+"""
+ASP home page.
+"""
+
+import streamlit as st
+
+
+def home_page() -> None:
+    """render home page"""
+
+    st.title("🚗 - Accident Severity Predictor (ASP)")
+    st.markdown("---")
